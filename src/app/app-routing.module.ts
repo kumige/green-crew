d91acbd0e98c50898379ebb75f5c9e7a4eedc14c
Mikko Romo <mikko1.romo@gmail.com>
2019-02-25 13:35:07 +0200

    added api routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 const routes: Routes = [
   { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" },
   { path: "login", loadChildren: "./login/login.module#LoginPageModule" },
-  { path: "", loadChildren: "./tabs/tabs.module#TabsPageModule" },
   {
     path: "media-provider",
     loadChildren:
--- src/app/media-provider/media-provider.page.ts
+++ src/app/media-provider/media-provider.page.ts
@@ -1,6 +1,12 @@
+import {
+  User,
+  LoginResponse,
+  RegisterResponse,
+  UserCheck
+} from "./../interfaces/user";
 import { IPic } from "./../interfaces/file";
 import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
   selector: "app-media-provider",
@@ -16,9 +22,152 @@ export class MediaProviderPage implements OnInit {
     return this.http.get<IPic[]>("http://media.mw.metropolia.fi/wbma/media");
   }
 
+  getUserMedia() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.get<IPic[]>(
+      "http://media.mw.metropolia.fi/wbma/media/user",
+      httpOptions
+    );
+  }
+
   getSingleMedia(id) {
     return this.http.get<IPic>(
       "http://media.mw.metropolia.fi/wbma/media/" + id
+    );
+  }
+
+  getProfilePic(tag: string) {
+    return this.http.get("http://media.mw.metropolia.fi/wbma/tags/" + tag);
+  }
+
+  getProfileData() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.get<User>(
+      "http://media.mw.metropolia.fi/wbma/users/user",
+      httpOptions
+    );
+  }
+
+  getUserData(id) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.get<User>(
+      "http://media.mw.metropolia.fi/wbma/users/" + id,
+      httpOptions
+    );
+  }
+
+  updateFile(fileID, data) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.put(
+      "http://media.mw.metropolia.fi/wbma/media/" + fileID,
+      data,
+      httpOptions
+    );
+  }
+
+  deleteFile(fileID) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.delete(
+      "http://media.mw.metropolia.fi/wbma/media/" + fileID,
+      httpOptions
+    );
+  }
+
+  login(user: User) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+    return this.http.post<LoginResponse>(
+      "http://media.mw.metropolia.fi/wbma/login",
+      user,
+      httpOptions
+    );
+  }
+
+  register(user: User) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+    return this.http.post<RegisterResponse>(
+      "http://media.mw.metropolia.fi/wbma/users",
+      user,
+      httpOptions
+    );
+  }
+
+  userCheck(user: User) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    };
+    return this.http.get<UserCheck>(
+      "http://media.mw.metropolia.fi/wbma/users/username/" + user.username,
+      httpOptions
+    );
+  }
+
+  upload(data: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.post(
+      "http://media.mw.metropolia.fi/wbma/media",
+      data,
+      httpOptions
+    );
+  }
+
+  addTag(data: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.post(
+      "http://media.mw.metropolia.fi/wbma/tags",
+      data,
+      httpOptions
+    );
+  }
+
+  getTags(id) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "x-access-token": localStorage.getItem("token")
+      })
+    };
+    return this.http.post(
+      "http://media.mw.metropolia.fi/wbma/tags/file/" + id,
+      httpOptions
     );
   }
 }
