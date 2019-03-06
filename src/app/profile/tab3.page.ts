ce023b03f475623625470fefce81a1dc675664ab
JanneSuotsalo <janne.suotsalo@gmail.com>
2019-03-06 13:30:33 +0200

    added profile functionality

--- src/app/profile/tab3.page.ts
+++ src/app/profile/tab3.page.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { MediaProviderPage } from "../media-provider/media-provider.page";
 import { NavController } from "@ionic/angular";
+import { Profile } from "../interfaces/user";
+import { element } from "@angular/core/src/render3";
 
 @Component({
   selector: "app-tab3",
@@ -12,6 +14,33 @@ export class Tab3Page {
     public mediaProvider: MediaProviderPage,
     public navCtrl: NavController
   ) {}
+
+  profileArray: Profile = { username: null };
+  mediaUrl = "http://media.mw.metropolia.fi/wbma/uploads/";
+
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter profile");
+    this.getUserData();
+  }
+
+  getUserData() {
+    this.mediaProvider.getProfileData().subscribe(res => {
+      console.log(res);
+      this.profileArray = res;
+
+      this.mediaProvider.getProfilePic("profile").subscribe((res2: any[]) => {
+        console.log(res2);
+        res2.forEach(element => {
+          if (element.user_id === this.profileArray.user_id) {
+            console.log("element " + element);
+            let newUrl = this.mediaUrl + element.filename;
+            console.log("newurl " + newUrl);
+            this.profileArray.filename = newUrl;
+          }
+        });
+      });
+    });
+  }
 
   logout() {
     localStorage.removeItem("token");
